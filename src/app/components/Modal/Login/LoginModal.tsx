bc118709fbe68ed7efa7b7aee7252293a82002d5
charlenefialho <fialhocharlene@gmail.com>
2023-07-17 16:51:23 -0300

    wip: LogInModal firebase authentication

--- src/app/components/Modal/Login/LoginModal.tsx
+++ src/app/components/Modal/Login/LoginModal.tsx
@@ -11,6 +11,7 @@ import padlockIcon from "../../../assets/svg//pad-lock-icon.svg";
 import { Background, Container, Content, ModalHeader } from "../Modal.styles";
 import { Button } from "../../Button/Button";
 import { RegisterModal } from "../Register/RegisterModal";
+import { login } from "../../../../../utils/firebase/authService";
 
 export interface Modal {
   isOpen: boolean;
@@ -22,8 +23,13 @@ export function LoginModal({ isOpen, setOpen }: Modal) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e: FormEvent) => {
+  const handlerLogin = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const form  = e.target as HTMLFormElement;
+    const emailInput = form.elements.namedItem('email') as HTMLInputElement;
+    const passwordInput = form.elements.namedItem('password') as HTMLInputElement;
+
+    login(emailInput.value, passwordInput.value).then((user) => console.log(user)).catch((err) => console.log(err));
   };
 
   if (isOpen) {
@@ -41,15 +47,15 @@ export function LoginModal({ isOpen, setOpen }: Modal) {
               Entre na sua conta para continuar o processo
             </p>
 
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handlerLogin}>
               <S.InputModal>
-                <S.InputContent placeholder="E-mail" type="email" />
+                <S.InputContent placeholder="E-mail" type="email" id="email"/>
                 <S.IconSpan>
                   <Image src={emailIcon} width={24} height={24} alt="" />
                 </S.IconSpan>
               </S.InputModal>
               <S.InputModal>
-                <S.InputContent placeholder="Senha" type="password" />
+                <S.InputContent placeholder="Senha" type="password" id="password"/>
                 <S.IconSpan>
                   <Image src={padlockIcon} width={24} height={24} alt="" />
                 </S.IconSpan>
@@ -58,7 +64,7 @@ export function LoginModal({ isOpen, setOpen }: Modal) {
                 <input type="checkbox" name="useTerms" id="useTerms" /> Lembra
                 minha conta
               </section>
-              <Button content="Criar" styles={S.buttonStyleModal} visible />
+              <Button type="submit" content="Criar" styles={S.buttonStyleModal} visible />
             </form>
 
             <div className="divTextQuestion">
