c427007f9960e39967278a3122091451775bf43e
charlenefialho <fialhocharlene@gmail.com>
2023-07-18 09:33:42 -0300

    feat: inputValidations register and login

--- src/app/components/Modal/Login/LoginModal.tsx
+++ src/app/components/Modal/Login/LoginModal.tsx
@@ -3,6 +3,9 @@
 import React, { useState, FormEvent } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import * as S from "../Modal.styles";
 import xCircle from "../../../assets/svg/icon-x-circle.svg";
@@ -12,25 +15,42 @@ import { Background, Container, Content, ModalHeader } from "../Modal.styles";
 import { Button } from "../../Button/Button";
 import { RegisterModal } from "../Register/RegisterModal";
 import { login } from "../../../../../utils/firebase/authService";
+import { errorToJSON } from "next/dist/server/render";
 
 export interface Modal {
   isOpen: boolean;
   setOpen: (isOpen: boolean) => void;
 }
 
+interface UserLogin {
+  email: string,
+  password: string
+}
+
+const loginFormValidationSchema = zod.object({
+  email: zod
+    .string()
+    .nonempty("Insira o email")
+    .email("Informe um e-mail válido"),
+  password: zod.string().nonempty("Insira a senha")
+});
+
+type loginUserData = zod.infer<typeof loginFormValidationSchema>;
+
 export function LoginModal({ isOpen, setOpen }: Modal) {
   const [modalRegisterOpen, setModalRegisterOpen] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handlerLogin = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form  = e.target as HTMLFormElement;
-    const emailInput = form.elements.namedItem('email') as HTMLInputElement;
-    const passwordInput = form.elements.namedItem('password') as HTMLInputElement;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginUserData>({
+    resolver: zodResolver(loginFormValidationSchema),
+  });
 
-    login(emailInput.value, passwordInput.value).then((user) => console.log(user)).catch((err) => console.log(err));
-  };
+  function handleSubmitLogin(data: UserLogin) {
+    
+  }
 
   if (isOpen) {
     return (
@@ -47,36 +67,66 @@ export function LoginModal({ isOpen, setOpen }: Modal) {
               Entre na sua conta para continuar o processo
             </p>
 
-            <form onSubmit={handlerLogin}>
+            <form onSubmit={handleSubmit(handleSubmitLogin)}>
               <S.InputModal>
-                <S.InputContent placeholder="E-mail" type="email" id="email"/>
+                <S.InputContent
+                  placeholder="E-mail"
+                  type="text"
+                  id="email"
+                  {...register("email")}
+                />
                 <S.IconSpan>
                   <Image src={emailIcon} width={24} height={24} alt="" />
                 </S.IconSpan>
               </S.InputModal>
+              {errors.email && (
+                <span style={{ color: "#ff0000" }}>{errors.email.message}</span>
+              )}
               <S.InputModal>
-                <S.InputContent placeholder="Senha" type="password" id="password"/>
+                <S.InputContent
+                  placeholder="Senha"
+                  type="password"
+                  id="password"
+                  {...register("password")}
+                />
                 <S.IconSpan>
                   <Image src={padlockIcon} width={24} height={24} alt="" />
                 </S.IconSpan>
               </S.InputModal>
+              {errors.password && (
+                <span style={{ color: "#ff0000" }}>
+                  {errors.password.message}
+                </span>
+              )}
               <section className="useTermsCheckBox">
                 <input type="checkbox" name="useTerms" id="useTerms" /> Lembra
                 minha conta
               </section>
-              <Button type="submit" content="Criar" styles={S.buttonStyleModal} visible />
+              <Button
+                type="submit"
+                content="Criar"
+                styles={S.buttonStyleModal}
+                visible
+              />
             </form>
 
             <div className="divTextQuestion">
               <span className="textQuestion">
                 Não tem uma conta?{" "}
-                <Link href="#" onClick={() => setModalRegisterOpen(!modalRegisterOpen)} className="linkTextQuestion">
+                <Link
+                  href="#"
+                  onClick={() => setModalRegisterOpen(!modalRegisterOpen)}
+                  className="linkTextQuestion"
+                >
                   Cadastrar
                 </Link>
               </span>
             </div>
 
-            <RegisterModal isOpen={modalRegisterOpen} setOpen={setModalRegisterOpen} />
+            <RegisterModal
+              isOpen={modalRegisterOpen}
+              setOpen={setModalRegisterOpen}
+            />
           </Content>
         </Container>
       </Background>
--- src/app/components/Modal/Register/RegisterModal.tsx
+++ src/app/components/Modal/Register/RegisterModal.tsx
@@ -4,6 +4,9 @@ import React, { useState, FormEvent } from "react";
 import { User, WarningCircle } from "@phosphor-icons/react";
 import Link from "next/link";
 import Image from "next/image";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import * as S from "../Modal.styles";
 import xCircle from "../../../assets/svg/icon-x-circle.svg";
@@ -17,16 +20,37 @@ export interface Modal {
   setOpen: (isOpen: boolean) => void;
 }
 
+const registerFormValidationSchema = zod.object({
+  userName: zod.string().nonempty("O nome é obrigatório"),
+  email: zod
+    .string()
+    .min(1, "Informe o e-mail")
+    .email("Informe um e-mail válido"),
+  password: zod
+    .string({
+      required_error: "Campo de senha é obrigatório",
+    })
+    .min(8, "A senha deve ter no mínimo 8 caracteres")
+    .regex(
+      /^(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$/,
+      "A senha deve conter no mínimo um número e um caractere especial"
+    ),
+});
+
+type registerUserData = zod.infer<typeof registerFormValidationSchema>;
+
 export function RegisterModal({ isOpen, setOpen }: Modal) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const handleSubmitRegister = (data: any) => {};
 
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<registerUserData>({
+    resolver: zodResolver(registerFormValidationSchema),
+  });
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-  };
+  console.log(errors);
 
   if (isOpen) {
     return (
@@ -39,31 +63,56 @@ export function RegisterModal({ isOpen, setOpen }: Modal) {
                 <Image src={xCircle} alt="" />
               </button>
             </ModalHeader>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(handleSubmitRegister)}>
               <S.InputModal>
-                <S.InputContent placeholder="Nome Completo" type="text" />
+                <S.InputContent
+                  placeholder="Nome Completo"
+                  type="text"
+                  {...register("userName")}
+                />
                 <S.IconSpan>
                   <User size={24} />
                 </S.IconSpan>
               </S.InputModal>
-
+              {errors.userName && (
+                <span style={{ color: "#ff0000" }}>
+                  {errors.userName.message}
+                </span>
+              )}
               <S.InputModal>
-                <S.InputContent placeholder="E-mail" type="email" />
+                <S.InputContent
+                  placeholder="E-mail"
+                  type="text"
+                  {...register("email")}
+                />
                 <S.IconSpan>
                   <Image src={emailIcon} width={24} height={24} alt="" />
                 </S.IconSpan>
               </S.InputModal>
+              {errors.email && (
+                <span style={{ color: "#ff0000" }}>{errors.email.message}</span>
+              )}
               <S.InputModal>
-                <S.InputContent placeholder="Senha" type="password" />
+                <S.InputContent
+                  placeholder="Senha"
+                  type="password"
+                  {...register("password")}
+                />
                 <S.IconSpan>
                   <Image src={padlockIcon} width={24} height={24} alt="" />
                 </S.IconSpan>
               </S.InputModal>
+              {errors.password && (
+                <span style={{ color: "#ff0000" }}>
+                  {errors.password.message}
+                </span>
+              )}
               <span>
                 <WarningCircle size={16} />
                 Mínimo de 8 caracteres, no mínimo um número e um caractere
                 especial
               </span>
+
               <S.InputModal>
                 <S.InputContent
                   placeholder="Confirmação de senha"
