866dff9f57f03d0be5d362ae31350f9452c01f72
charlenefialho <fialhocharlene@gmail.com>
2023-07-19 16:00:15 -0300

    feat: adds newSuggest

--- src/app/components/Journey/Modal/CriarSugestao/index.tsx
+++ src/app/components/Journey/Modal/CriarSugestao/index.tsx
@@ -1,45 +1,92 @@
-'use client';
+"use client";
 import React from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import { Button } from "@/app/components/Button/Button";
 import { GlobalModal } from "../GlobalModal";
 import { InputContent, InputModal } from "../GlobalModal.styles";
+import { getDatabase, ref, push, set } from "firebase/database";
+import { toast } from "react-toastify";
 
-interface ICreateSuggest{
-  id?:React.Key;
-  nameTable: string
+interface ICreateSuggest {
+  id?: React.Key;
+  nameTable: string;
 }
 
-export function CriarSugestao({id, nameTable}: ICreateSuggest) {
+const createSuggestValidationSchema = zod.object({
+  title: zod.string().nonempty("Insira o título"),
+  description: zod.string().nonempty("Insira a descrição"),
+});
+
+type newSuggestData = zod.infer<typeof createSuggestValidationSchema>;
+
+export function CriarSugestao({ id, nameTable }: ICreateSuggest) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<newSuggestData>({
+    resolver: zodResolver(createSuggestValidationSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  async function handleSubmitNewSuggest(data: newSuggestData) {
+    const formattedNameTable = `${nameTable
+      .toUpperCase()
+      .replace(/\s+/g, "_")
+      .replace(/_+$/, "")}`;
+    try {
+      const db = getDatabase();
+      const suggestListRef = ref(db, `${formattedNameTable}`);
+      const newsuggestRef = push(suggestListRef);
+      set(newsuggestRef, {
+        id_jornada: id,
+        titulo: data.title,
+        descricao: data.description,
+      });
+      reset();
+      toast.success("Sugestão adicionada com sucesso");
+    } catch (erro) {
+      toast.error("Ocorreu um erro");
+    }
+  }
+
   return (
     <GlobalModal titleModal="Criar uma sugestão">
-      <form action="">
+      <form onSubmit={handleSubmit(handleSubmitNewSuggest)}>
         <InputModal>
           <InputContent
             placeholder="Título"
             style={{ paddingLeft: "1rem", paddingRight: "2.6rem" }}
+            {...register("title")}
           />
         </InputModal>
         <textarea
-          name="descricaoSugestao"
           id="descricaoSugestao"
           className="textAreaCriarSugestao"
           cols={30}
           rows={10}
           placeholder="Descrição..."
+          {...register("description")}
         ></textarea>
         <div
           style={{ display: "flex", justifyContent: "center", width: "100%" }}
         >
           <div style={{ display: "flex", gap: "1rem", width: "100%" }}>
             <Dialog.Close asChild>
-                <Button
-                  content="Cancelar"
-                  styles={{ width: "100%", height: "56px", padding: "20px 0px"}}
-                  visible
-                  light
-                />
+              <Button
+                content="Cancelar"
+                styles={{ width: "100%", height: "56px", padding: "20px 0px" }}
+                visible
+                light
+              />
             </Dialog.Close>
             <Button
               type="submit"
