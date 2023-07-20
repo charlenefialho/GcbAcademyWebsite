f379f4d743146828a4e611bac1132241e6639411
charlenefialho <fialhocharlene@gmail.com>
2023-07-20 10:41:43 -0300

    refactor: code eligibility AboutUs.tsx

--- src/app/components/AboutUs/AboutUs.tsx
+++ src/app/components/AboutUs/AboutUs.tsx
@@ -1,9 +1,49 @@
 "use client";
-
 import React from "react";
-
 import * as S from "./AboutUs.styles";
 import imgAboutUs from "../../assets/svg/about-us.svg";
+
+const AboutUsTextSection = () => {
+  return (
+    <S.SectionTextAboutUs>
+      <div className="contentTextAboutUs">
+        <div>
+          <p>
+            O{" "}
+            <span>
+              <strong>GCB Academy</strong>
+            </span>{" "}
+            nasceu da <strong>necessidade</strong> de{" "}
+            <strong>pessoas qualificadas</strong> na área de tecnologia do Grupo
+            GCB a partir do alinhamento cultural e técnico com a empresa.
+          </p>
+        </div>
+        <div>
+          <p>
+            Com o Academy abrimos as portas para o futuro, preparando os
+            profissionais através de muita <strong>prática</strong> e{" "}
+            <strong>cenários reais</strong>, a qual impulsionamos o crescimento
+            e evolução.{" "}
+          </p>
+        </div>
+        <div>
+          <p>
+            Prepare-se para explorar novas fronteiras, desafiar limites e
+            alcançar o próximo nível do seu potencial.
+          </p>
+        </div>
+      </div>
+    </S.SectionTextAboutUs>
+  );
+};
+
+const AboutUsImageSection = () => {
+  return (
+    <S.DivImgAboutUS>
+      <S.ImgAboutUsStyled src={imgAboutUs} alt="pessoa pesquisando" />
+    </S.DivImgAboutUS>
+  );
+};
 
 export function SobreNos() {
   return (
@@ -12,39 +52,8 @@ export function SobreNos() {
         <S.TitleAboutUs>Sobre nós</S.TitleAboutUs>
       </div>
       <S.ContainerAbouUs>
-        <S.SectionTextAboutUs>
-          <div className="contentTextAboutUs">
-            <div>
-              <p>
-                O{" "}
-                <span>
-                  <strong>GCB Academy</strong>
-                </span>{" "}
-                nasceu da <strong>necessidade</strong> de
-                <strong> pessoas qualificadas</strong> na área de tecnologia do
-                Grupo GCB a partir do alinhamento cultural e técnico com a
-                empresa.
-              </p>
-            </div>
-            <div>
-              <p>
-                Com o Academy abrimos as portas para o futuro, preparando os
-                profissionais através de muita <strong>prática</strong> e{" "}
-                <strong>cenários reais</strong>, a qual impusionamos o
-                crescimento e evolução.{" "}
-              </p>
-            </div>
-            <div>
-              <p>
-                Prepare-se para explorar novas fronteiras, desafiar limites e
-                alcançar o próximo nível do seu potencial.
-              </p>
-            </div>
-          </div>
-        </S.SectionTextAboutUs>
-        <S.DivImgAboutUS>
-          <S.ImgAboutUsStyled src={imgAboutUs} alt="pessoa pesquisando" />
-        </S.DivImgAboutUS>
+        <AboutUsTextSection />
+        <AboutUsImageSection />
       </S.ContainerAbouUs>
     </S.SectionAboutUs>
   );
