64881e8ac397d3b2426b2e373a043c070e888e38
charlenefialho <fialhocharlene@gmail.com>
2023-07-21 07:27:36 -0300

    test: story default of newsletter

---
+++ src/app/components/Newsletter/Newsletter.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Newsletter } from "./Newsletter";
+
+export default {
+    title: 'Components/Newsletter',
+    component: Newsletter
+}as Meta
+
+export const Default: StoryObj={}
