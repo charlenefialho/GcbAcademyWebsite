854a0866f92a09cdf5f54de4a6a0d9d8cc7057be
charlenefialho <fialhocharlene@gmail.com>
2023-07-21 06:16:22 -0300

    test: story of AboutUs

---
+++ src/app/components/AboutUs/AboutUs.stories.tsx
@@ -0,0 +1,10 @@
+
+import { Meta, StoryObj } from "@storybook/react";
+import { AboutUs } from "./AboutUs";
+
+export default {
+    title: 'AboutUs',
+    component: AboutUs
+}as Meta
+
+export const Default: StoryObj={}
